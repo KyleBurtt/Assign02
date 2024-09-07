64ac7153eb15b8ea7d97bc96e9f7324644e5a17f
KyleBurtt <kyleaburtt@gmail.com>
2024-09-06 21:10:56 -0600

    First commit

--- obj/Debug/net8.0/Assign02.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assign02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assign02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a42208103fae1de8d2c01906ba8b4e90f6a72dec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assign02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assign02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
